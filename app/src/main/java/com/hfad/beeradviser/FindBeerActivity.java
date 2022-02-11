d2709612505938e3323262975cc0600a92c53766
admin <g.lyalko@mail.ru>
2022-02-12 00:08:11 +0300

    Начал работу с активностью

--- app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
@@ -3,6 +3,7 @@ package com.hfad.beeradviser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class FindBeerActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class FindBeerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+    //Called when the user clicks the button
+    public void onClickFindBeer(View view){
     }
 }
